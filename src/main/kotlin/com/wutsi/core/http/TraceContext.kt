17971f49eb666d1c528ee533b914778d04995f70
wutsi <herve.tchepannou@wutsi.com>
2022-07-10 02:03:09 -0400

    rename the device  id header

--- src/main/kotlin/com/wutsi/core/http/TraceContext.kt
+++ src/main/kotlin/com/wutsi/core/http/TraceContext.kt
@@ -6,12 +6,12 @@ interface TraceContext {
         const val MESSAGE_ID = "X-Message-ID"
         const val PARENT_MESSAGE_ID = "X-Parent-Message-ID"
         const val CLIENT_ID = "X-Client-ID"
-        const val DEVICE_UID = "X-Device-UID"
+        const val DEVICE_UID = "X-Device-ID"
         const val USER_AGENT = "User-Agent"
     }
 
     fun clientId(): String
-    fun deviceUid() : String?
+    fun deviceUid(): String?
     fun traceId(): String?
     fun messageId(): String?
     fun parentMessageId(): String?
